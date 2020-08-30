a973acc44b9551a9114a2f14a1582b63356eee6d
haz <hani.abuzeid@columbusglobal.com>
2020-08-30 15:27:30 +0200

    feature/01-initial
    - Added different pages
    - :art: global styles
    - :file_folder: for [screens]

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	export let name;
+    import Welcome from './screens/Welcome.svelte';
+    let state = 'welcome'; // or 'playing' is the alternative
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 800px;
+        height: 100%;
+        margin: 0 auto;
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+    }
 </style>
+
+<main>
+    {#if state == 'welcome'}
+        <Welcome />
+    {:else if state == 'playing'}
+        <p>game screen goes here</p>
+    {/if}
+</main>
