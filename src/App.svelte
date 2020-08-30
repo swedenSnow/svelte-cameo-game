3bec6d23d427cb10d6dba999165a344ac9a64bd3
haz <hani.abuzeid@columbusglobal.com>
2020-08-30 17:04:51 +0200

    02-screens
    - Logic for diffrent screens
    - Fetching scraped data and deserializing

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,63 @@
 <script>
-    import Welcome from './screens/Welcome.svelte';
-    let state = 'welcome'; // or 'playing' is the alternative
+    import { onMount } from "svelte";
+    import Welcome from "./screens/Welcome.svelte";
+    import { select } from "./select.js";
+
+    let state = "welcome"; // or 'playing' is the alternative
+    let selection;
+
+    let celebs_promise;
+
+    const start = async (e) => {
+        console.log(e);
+        const { celebs, lookup } = await celebs_promise;
+
+        selection = select(celebs, lookup, e.detail.category.slug);
+
+        state = "playing";
+    };
+
+    const loads_celebs = async () => {
+        const res = await fetch(
+            "https://cameo-explorer.netlify.app/celebs.json"
+        );
+        const data = await res.json();
+
+        const lookup = new Map();
+
+        data.forEach((c) => {
+            lookup.set(c.id, c);
+        });
+        console.log(lookup);
+        console.log(data);
+        //! celebs we will have for the game
+        const subset = new Set();
+        data.forEach((c) => {
+            if (c.reviews >= 50) {
+                subset.add(c);
+                c.similar.forEach((id) => {
+                    subset.add(lookup.get(id));
+                });
+            }
+        });
+        console.log(subset);
+        return {
+            celebs: Array.from(subset),
+            lookup,
+        };
+    };
+    onMount(() => {
+        celebs_promise = loads_celebs();
+    });
 </script>
+
+<main>
+    {#if state == 'welcome'}
+        <Welcome on:select={start} />
+    {:else if state == 'playing'}
+        <p>game screen goes here</p>
+    {/if}
+</main>
 
 <style>
     main {
@@ -15,11 +71,3 @@
         justify-content: center;
     }
 </style>
-
-<main>
-    {#if state == 'welcome'}
-        <Welcome />
-    {:else if state == 'playing'}
-        <p>game screen goes here</p>
-    {/if}
-</main>
--- src/screens/Welcome.svelte
+++ src/screens/Welcome.svelte
@@ -1,3 +1,50 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    let selected = false;
+
+    const select = (category) => {
+        selected = true;
+        dispatch("select", { category });
+        // console.log("selected", category);
+    };
+    const categories = [
+        { slug: "actors", label: "Actors" },
+        { slug: "athletes", label: "Athletes" },
+        { slug: "comedians", label: "Comedians" },
+        { slug: "creators", label: "Creators" },
+        { slug: "models", label: "Models" },
+        { slug: "musicians", label: "Musicians" },
+        { slug: "reality-tv", label: "Reality TV" },
+    ];
+</script>
+
+<header>
+    <h1>
+        CameoP
+        <span class="logo">a</span>
+        rison
+    </h1>
+    <p>
+        On
+        <a href="https://cameo.com">cameo.com</a>
+        , you can buy personalised video clips from everyone from Lindsay Lohan
+        to Ice T.
+    </p>
+
+    <p>But who commands the highest price?</p>
+</header>
+<p>Pick a category to play a game:</p>
+
+<div class="categories">
+    {#each categories as category}
+        <button disabled={selected} on:click={() => select(category)}>
+            {category.label}
+        </button>
+    {/each}
+</div>
+
 <style>
     h1 {
         color: hsl(15deg 100% 50%);
@@ -21,20 +68,3 @@
         text-indent: -9999px;
     }
 </style>
-
-<header>
-    <h1>
-        CameoP
-        <span class="logo">a</span>
-        rison
-    </h1>
-
-    <p>
-        On
-        <a href="https://cameo.com">cameo.com</a>
-        , you can buy personalised video clips from everyone from Lindsay Lohan
-        to Ice T.
-    </p>
-
-    <p>But who commands the highest price?</p>
-</header>
